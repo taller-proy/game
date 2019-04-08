bd344c8005101b385a50df0724f7a9969693fc1d
Cesar Quesada <43500882+cj3096@users.noreply.github.com>
2019-04-07 22:35:26 -0500

    burbujas de dialogo
    burbujas de dialogo

---
+++ Assets/texto.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class texto : MonoBehaviour
+{
+    public GameObject voltimetro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        voltimetro.SetActive(false);
+    }
+
+    public void OnMouseOver()
+    {
+        voltimetro.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        voltimetro.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
